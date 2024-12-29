e3ccf7664bdbd77a6d2be0f084a8e79f930de178
35402 <3540291485@qq.com>
2024-12-29 11:28:54 +0800

    12月29日苏伟翰

--- src/main/java/com.group12/config/MybatisPlusConfig.java
+++ src/main/java/com.group12/config/MybatisPlusConfig.java
@@ -1,4 +1,20 @@
 package com.group12.config;
 
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Bean;
+
+@Cacheable
+@MapperScan("com.group12.service")
 public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
 }
--- src/main/java/com.group12/controller/ExamProCont.java
+++ src/main/java/com.group12/controller/ExamProCont.java
@@ -1,12 +1,17 @@
 package com.group12.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.group12.entity.ExamProEntity;
+import com.group12.entity.ExamProSearch;
 import com.group12.service.ExamProService;
 import com.group12.util.ResultUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Slf4j
@@ -53,5 +58,37 @@ public class ExamProCont {
         }
     }
 
-
+    //多条件分页查询
+    @PostMapping("/getProByCon")
+    public Object getProByCon(@RequestBody ExamProSearch examProSearch) {
+        Page<ExamProEntity> page = new Page<>(examProSearch.getPageNo(),examProSearch.getPageSize());
+        QueryWrapper<ExamProEntity> queryWrapper = new QueryWrapper<>();
+        if(examProSearch.getProbody() != null && !examProSearch.getProbody().equals("")){
+            //根据题干模糊查询
+            queryWrapper.like("probody",examProSearch.getProbody());
+        }
+        if(examProSearch.getId() != null && !examProSearch.getId().equals("")){
+            //根据id精确查询
+            queryWrapper.eq("id",examProSearch.getId());
+        }
+        if(examProSearch.getDiff() != null && !examProSearch.getDiff().equals("")){
+            //根据难易度精确查询
+            queryWrapper.eq("diff",examProSearch.getDiff());
+        }
+        if(examProSearch.getCourse() != null && !examProSearch.getCourse().equals("")){
+            //根据课程模糊查询
+            queryWrapper.like("course",examProSearch.getCourse());
+        }
+        if(examProSearch.getKnowpoint() != null && !examProSearch.getKnowpoint().equals("")){
+            //根据知识点模糊查询
+            queryWrapper.like("knowpoint",examProSearch.getKnowpoint());
+        }
+        if(examProSearch.getProtype() != null && !examProSearch.getProtype().equals("")){
+            //根据题目类型查询
+            queryWrapper.like("protype",examProSearch.getProtype());
+        }
+        IPage<ExamProEntity> iPage = examProChooseService.page(page, queryWrapper);
+        System.out.println("总记录数："+iPage.getRecords());
+        return ResultUtil.success(iPage);
+    }
 }
